5e5c52566b99d80d9ffafa9428603db4a2ed9261
Andrea C <acgarabito2@gmail.com>
2020-04-21 12:31:02 +0200

    borrar nota + estilos

--- app/scripts/asideLeftNotes.js
+++ app/scripts/asideLeftNotes.js
@@ -3,10 +3,10 @@ function add_note() {
 
     if(new_note.length > 0) {
         let li = document.createElement('li');
-
-        li.id = new_note;
-        li.innerHTML = new_note;
         
+        li.id = new_note;
+        li.innerHTML = new_note + "   <span onclick='remove_note(this)'>borrar</span>";
+
         document.getElementById("notes_list").appendChild(li);
     }
 
@@ -14,7 +14,8 @@ function add_note() {
 }
  
 function remove_note(element) {
-   var id = element.parentNode.getAttribute("id");
+   let id = element.parentNode.getAttribute("id");
+
    node = document.getElementById(id);
    node.parentNode.removeChild(node);
 }
@@ -23,16 +24,20 @@ window.onload = function() {
     let aside = document.getElementById("aside_left_notes");
 
     let container = document.createElement("div");
+    let h1 = document.createElement("h1");
     let input = document.createElement("input");
     let button = document.createElement("button");
     let list = document.createElement("ul");
 
+    container.setAttribute("id", "container_notes");
+    h1.innerHTML = "Mis Notas";
     input.setAttribute("id", "new_note");
     button.setAttribute("id", "add_note");
     button.innerHTML = "Crear Nota";
     list.setAttribute("id", "notes_list");
 
     aside.appendChild(container);
+    container.appendChild(h1);
     container.appendChild(input);
     container.appendChild(button);
     container.appendChild(list);
