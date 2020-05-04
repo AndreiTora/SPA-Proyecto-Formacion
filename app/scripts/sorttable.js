5f4e0a4315e9eeafafec8a5883fcb9dc86b3370c
Andrea C <acgarabito2@gmail.com>
2020-05-04 13:35:45 +0200

    ordenar asc/desc

--- app/scripts/sorttable.js
+++ app/scripts/sorttable.js
@@ -1,9 +1,21 @@
+let estado = false;
+
 export const sortAlpha = (array, property) => {
+    if (estado === true) {
+        estado = false;
     return array.sort(function (a,b) {
         if(a[property].toLowerCase() < b[property].toLowerCase()) return -1;
         if(a[property].toLowerCase() > b[property].toLowerCase()) return 1;
         return 0;
     })
+    }else {
+        estado = true;
+        return array.sort(function (a,b) {
+            if(a[property].toLowerCase() < b[property].toLowerCase()) return -1;
+            if(a[property].toLowerCase() > b[property].toLowerCase()) return 1;
+            return 0;
+        }).reverse();
+    }
 }
 
 export const sortTable = (document, property) => {  
